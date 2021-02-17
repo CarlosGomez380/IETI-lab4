5746028a3f3ff8c465df0316d8c4be6ddca56dbc
jmrodriguezp <gemec98@gmail.com>
2021-02-16 22:16:55 -0500

    service worker

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {Login} from './components/Login';
 import TodoPlanner from './components/TodoPlanner';
@@ -7,7 +7,7 @@ import ResponsiveDrawer from './components/drawer'
 function App() {
   localStorage.setItem('username', "david");
   localStorage.setItem('password', 123);
-  var toShow= "";
+
   if(localStorage.getItem('isLoggedIn') != "true"){
     return (
       <div className="App">
--- src/components/drawer.js
+++ src/components/drawer.js
@@ -6,19 +6,16 @@ import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import OutlinedCard from './card';
 
 const drawerWidth = 240;
 
